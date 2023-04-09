127046affddd2a05f167f2685ac0619dfcaa8beb
Titus Lim <tituslhy@gmail.com>
2023-04-09 14:38:05 +0800

    Update device agnostic function

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,8 @@
 import torch
 
-FORCE_CPU = False
 def get_pytorch_device():
-    if not FORCE_CPU:
-        if torch.cuda.is_available():
-            return 'cuda'
-        elif torch.backends.mps.is_available():
-            return 'mps'
-    
+    if torch.cuda.is_available():
+        return 'cuda'
+    elif torch.backends.mps.is_available():
+        return 'mps'
     return 'cpu'
