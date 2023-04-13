2460654a16f3f04578efc018f735e8a56da30821
Titus Lim <tituslhy@gmail.com>
2023-04-13 22:58:23 +0800

    Created classification notebook

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,15 @@
 import torch
 
 def get_device():
+    """Detects accelerator device. The possible devices are:
+    1. cuda - this is preferred
+    2. mps - this is for mac devices
+    3. cpu - this is returned if there are no accelerator devices
+    available.
+
+    Returns:
+        str: name of accelerator device available.
+    """
     if torch.cuda.is_available():
         return 'cuda'
     elif torch.backends.mps.is_available():
@@ -12,3 +21,8 @@ def train():
 
 def evaluate():
     pass
+
+def classification_accuracy(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    return (correct/len(y_pred))*100
+
