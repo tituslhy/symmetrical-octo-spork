0da5347e5e63370f51644375575d045967c3c6f3
Titus Lim <tituslhy@gmail.com>
2023-04-23 21:59:22 +0800

    Updated CV notebook

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import torch
+from timeit import default_timer as timer
+
 
 def get_device():
     """Detects accelerator device. The possible devices are:
@@ -16,13 +18,13 @@ def get_device():
         return 'mps'
     return 'cpu'
 
-def train():
-    pass
-
-def evaluate():
-    pass
-
 def classification_accuracy(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
     return (correct/len(y_pred))*100
 
+def print_train_time(start: float,
+                     end: float,
+                     device: torch.device = None):
+    """Prints difference between start and end time"""
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds")
