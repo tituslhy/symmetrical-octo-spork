74e3503c6e0754e96b8c020a86f39df3ff7c5f6c
Titus Lim <tituslhy@gmail.com>
2023-05-06 22:00:45 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -191,8 +191,13 @@ def make_predictions(model:torch.nn.Module,
 def save_model(root_dir: str,
                model_name: str,
                model: torch.nn.Module):
-    
+    """Saves a torch model to the given root directory
 
+    Args:
+        root_dir (str): Name of directory to store model
+        model_name (str): Name of the model
+        model (torch.nn.Module): Torch model object
+    """
     MODEL_PATH = Path(root_dir)
     MODEL_PATH.mkdir(parents=True,
                      exist_ok = True)
