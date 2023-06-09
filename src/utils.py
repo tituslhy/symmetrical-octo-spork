2ba1905a5ca70f23ff0ebf547b02d32a697aae49
Titus Lim <tituslhy@gmail.com>
2023-06-09 15:25:12 +0800

    trained efficientnetb0

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,8 @@ from PIL import Image
 import torch
 from torch import nn
 
+import matplotlib.pyplot as plt
+
 def get_device():
     """Detects accelerator device. The possible devices are:
     1. cuda - this is preferred
@@ -35,176 +37,41 @@ def print_train_time(start: float,
     total_time = end - start
     print(f"Train time on {device}: {total_time:.3f} seconds")
 
-# def train_step(model: torch.nn.Module,
-#                dataloader: torch.utils.data.DataLoader,
-#                loss_fn: torch.nn.Module,
-#                optimizer: torch.optim.Optimizer,
-#                device: str = get_device()) -> Tuple[float, float]:
-#     """Helper function to train pytorch model on device
-#     and acquire training metrics per epoch
+def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
 
-#     Args:
-#         model (torch.nn.Module): instantiated torch model
-#         dataloader (torch.utils.data.DataLoader)
-#         loss_fn (torch.nn.Module)
-#         optimizer (torch.optim.Optimizer) 
-#         device (str, optional): Defaults to device.
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "test_loss": [...],
+             "test_acc": [...]}
+    """
+    loss = results["train_loss"]
+    test_loss = results["test_loss"]
 
-#     Returns:
-#         Average training loss and training accuracy per epoch
-#     """
-    
-#     train_loss, train_acc = 0,0
-#     model.train()
+    accuracy = results["train_acc"]
+    test_accuracy = results["test_acc"]
 
-#     for batch, (X, y) in enumerate(dataloader):
-        
-#         # Forward pass
-#         X, y = X.to(device), y.to(device)
-#         y_pred = model(X) #logits
-#         loss = loss_fn(y_pred, y)
-#         train_loss += loss.item()
+    epochs = range(len(results["train_loss"]))
 
-#         # Backpropagation
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
+    plt.figure(figsize=(15, 7))
 
-#         # Compute metric across all batches
-#         y_pred_class = torch.argmax(
-#             torch.softmax(y_pred, dim = 1),
-#             dim = 1
-#         )
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label="train_loss")
+    plt.plot(epochs, test_loss, label="test_loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
 
-#         train_acc += (y_pred_class==y).sum().item()/len(y_pred)
-    
-#     # Adjust metrics to get average loss and accuracy per batch
-#     train_loss = train_loss / len(dataloader)
-#     train_acc = train_acc/len(dataloader)
-
-#     return train_loss, train_acc
-
-# def test_step(model: torch.nn.Module,
-#               dataloader: torch.utils.data.DataLoader,
-#               loss_fn: torch.nn.Module,
-#               device: str = get_device()) -> Tuple[float, float]:
-#     """Runs inference of trained model on test dataset per epoch
-#     and monitors model test metrics.
-
-#     Args:
-#         model (torch.nn.Module): instantiated torch model
-#         dataloader (torch.utils.data.DataLoader)
-#         loss_fn (torch.nn.Module)
-#         device (str, optional): Defaults to device.
-
-#     Returns:
-#         Average test loss and test accuracy per epoch
-#     """
-    
-#     test_loss, test_acc = 0,0
-#     model.eval()
-    
-#     with torch.inference_mode():
-#         for batch, (X, y) in enumerate(dataloader):
-            
-#             # Forward pass
-#             X, y = X.to(device), y.to(device)
-#             test_pred = model(X) #logits
-
-#             # Compute metrics 
-#             loss = loss_fn(test_pred, y)
-#             test_loss += loss.item()
-#             test_pred_class = torch.argmax(
-#                 torch.softmax(test_pred, dim = 1),
-#                 dim = 1
-#             )
-#             test_acc += (test_pred_class == y).sum().item()/len(test_pred_class)
-    
-#     # Adjust metrics to get average loss and accuracy per batch
-#     test_loss = test_loss/len(dataloader)
-#     test_acc = test_acc/len(dataloader)
-    
-#     return test_loss, test_acc
-
-
-# def train(model: torch.nn.Module,
-#           train_dataloader: torch.utils.data.DataLoader,
-#           test_dataloader: torch.utils.data.DataLoader,
-#           optimizer: torch.optim.Optimizer,
-#           epochs: int,
-#           loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),) -> Dict(List):
-#     """Wrapper function to train model over specified number of epochs,
-#     model, dataloaders, optimizer and loss function.
-
-#     Args:
-#         model (torch.nn.Module): instantiated torch model
-#         train_dataloader (torch.utils.data.DataLoader)
-#         test_dataloader (torch.utils.data.DataLoader)
-#         optimizer (torch.optim.Optimizer)
-#         loss_fn (torch.nn.Module, optional) Defaults to nn.CrossEntropyLoss().
-#         epochs (int): Number of epochs to train
-
-#     Returns:
-#         Dictionary of results
-#     """
-    
-#     # Create storage results dictionary
-#     results = defaultdict(list)
-
-#     # Loop through training and testing steps for number of epochs
-#     for epoch in tqdm(range(epochs)):
-#         train_loss, train_acc = train_step(model = model,
-#                                            dataloader = train_dataloader,
-#                                            loss_fn = loss_fn,
-#                                            optimizer = optimizer)
-#         test_loss, test_acc = test_step(model = model,
-#                                         dataloader = test_dataloader,
-#                                         loss_fn = loss_fn)
-#         print(
-#             f"Epoch: {epoch+1} | "
-#             f"train_loss: {train_loss:.4f} | "
-#             f"train_acc: {train_acc:.4f} | "
-#             f"test_loss: {test_loss:.4f} | "
-#             f"test_acc: {test_acc:.4f}"
-#         )
-
-#         results["train_loss"].append(train_loss)
-#         results["train_acc"].append(train_acc)
-#         results["test_loss"].append(test_loss)
-#         results["test_acc"].append(test_acc)
-    
-#     return results
-
-# def make_predictions_batch(model:torch.nn.Module,
-#                            data:list,
-#                            device: torch.device):
-#     """Generates a list of predictions given a list of data
-
-#     Args:
-#         model (torch.nn.Module): Model instance
-#         data (list): List of img tensors to run inference
-#         device (torch.device, optional): Defaults to device.
-
-#     Returns:
-#         torch.tensor: Tensor of probabilities
-#     """
-#     pred_probs = []
-#     model.eval()
-#     with torch.inference_mode():
-#         for sample in data:
-#             # Prep sample - add the batch dimension and pass to device
-#             sample = torch.unsqueeze(sample, dim=0).to(device)
-            
-#             # Forward pass
-#             pred_logit = model(sample)
-            
-#             #Get prediction orobability
-#             pred_prob = torch.softmax(pred_logit.squeeze(), dim = 0)
-            
-#             pred_probs.append(pred_prob.cpu())
-    
-#     #Stack pred_probs in list into tensor
-#     return torch.stack(pred_probs)
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label="train_accuracy")
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
 
 def save_model(root_dir: str,
                model_name: str,
