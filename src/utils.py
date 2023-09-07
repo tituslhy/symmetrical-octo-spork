b1a0a77359f3179594c578a2b15090b6db7ef436
Titus Lim <tituslhy@gmail.com>
2023-09-07 12:02:05 +0800

    torch2 experiment

--- src/utils.py
+++ src/utils.py
@@ -10,21 +10,7 @@ from torch import nn
 
 import matplotlib.pyplot as plt
 
-def get_device():
-    """Detects accelerator device. The possible devices are:
-    1. cuda - this is preferred
-    2. mps - this is for mac devices
-    3. cpu - this is returned if there are no accelerator devices
-    available.
-
-    Returns:
-        str: name of accelerator device available.
-    """
-    if torch.cuda.is_available():
-        return 'cuda'
-    elif torch.backends.mps.is_available():
-        return 'mps'
-    return 'cpu'
+ 
 
 def classification_accuracy(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
